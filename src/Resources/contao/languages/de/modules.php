e97f5d26af5a1c75f5792f07e67ebc54431c8e58
Marko Cupic <m.cupic@gmx.ch>
2020-07-14 21:51:07 +0200

    update

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -3,7 +3,7 @@
 /**
  * @copyright  Marko Cupic 2020 <m.cupic@gmx.ch>
  * @author     Marko Cupic
- * @package    Test Bundle
+ * @package    Hello Test Bundle
  * @licence    MIT
  * @see        https://github.com/markocupic/test-bundle
  *
---
+++ src/Resources/contao/languages/de/tl_test_bundle.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright  Marko Cupic 2020 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    Hello Test Bundle
+ * @licence    MIT
+ * @see        https://github.com/markocupic/test-bundle
+ *
+ */
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_test_bundle']['first_legend'] = "Basis Einstellungen";
+$GLOBALS['TL_LANG']['tl_test_bundle']['first_legend'] = "Erweiterte Einstellungen";
+
+/**
+ * Operations
+ */
+$GLOBALS['TL_LANG']['tl_test_bundle']['edit'] = ["Datensatz mit ID: %s bearbeiten", "Datensatz mit ID: %s bearbeiten"];
+$GLOBALS['TL_LANG']['tl_test_bundle']['delete'] = ["Datensatz mit ID: %s löschen", "Datensatz mit ID: %s löschen"];
+$GLOBALS['TL_LANG']['tl_test_bundle']['show'] = ["Datensatz mit ID: %s ansehen", "Datensatz mit ID: %s ansehen"];
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_test_bundle']['title'] = ["Name", "Geben Sie einen Namen ein"];
+$GLOBALS['TL_LANG']['tl_test_bundle']['selectField'] = ["Select Feld", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_test_bundle']['checkboxField'] = ["Chosen Feld", "Wählen Sie aus."];
+$GLOBALS['TL_LANG']['tl_test_bundle']['multitextField'] = ["Multitext Feld", "Geben Sie die Werte ein"];
+$GLOBALS['TL_LANG']['tl_test_bundle']['addSubpalette'] = ["Erweiterte Einstellungen aktivieren", "Hier können Sie die erweiterten Einstellungen aktivieren."];
+$GLOBALS['TL_LANG']['tl_test_bundle']['textareaField'] = ["Textarea", "Geben Sie einen Text ein"];
+
+/**
+ * References
+ */
+$GLOBALS['TL_LANG']['tl_test_bundle']['firstoption'] = "Erste Option";
+$GLOBALS['TL_LANG']['tl_test_bundle']['secondoption'] = "Zweite Option";
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_test_bundle']['customButton'] = "Custom Routine starten";
+
+
